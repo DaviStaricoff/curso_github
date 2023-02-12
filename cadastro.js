f958255c1cab1ff4f8ed1775cead2ef95de8f892
DaviStaricoff <davi.staricoff@brudam.com.br>
2023-02-12 13:25:33 -0300

    corrigindo e adicionando validacoes no email

--- cadastro.js
+++ cadastro.js
@@ -4,3 +4,23 @@ function signUp (email) {
 }
 
 //pegando email do jao e passando pra visualizar no atalho 590 eh neh
+
+function signUp (email) {
+    return email
+    
+}
+function statusEmail (email) {
+    return email
+    
+}
+function conta (email) {
+    return email
+    
+}
+
+  {
+    return console.error(erro);
+
+}
+
+erro = "erro ao acessar verifique sua conta"
